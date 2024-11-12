9793298dc203500578c561bdef040876b346437b
Edmilson <edcarvalho.dev@gmail.com>
2024-11-12 11:27:54 -0300

    test: prepara para criação de test request

---
+++ Test/Requests/AdministradorRequestTest.cs
@@ -0,0 +1,21 @@
+
+
+namespace Test.Requests;
+
+[TestClass]
+public class AdministradorRequestTest
+{
+    [TestMethod]
+    public void TestarGetSetPropiedades()
+    {
+        // Arrange
+        
+
+        // Act
+        
+
+        // Assert
+        
+
+    }
+}
